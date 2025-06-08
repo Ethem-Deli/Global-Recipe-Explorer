3845021d26194233bce35c3a2a226d32d41501e0
Ethem-deli <del22021@byui.edu>
2025-06-08 15:36:27 +0300

    fo deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 // vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/Global-Recipe-Explorer/',  // 👈 This is the key fix
     build: {
         outDir: 'build'
     }
 });
 
+//1
+//import { defineConfig } from 'vite';
 
+//export default defineConfig({
+//    build: {
+//       outDir: 'build'
+//    }
+//});
+//2
 //import { defineConfig } from 'vite';
 //export default defineConfig({
 //    base: '/Global-Recipe-Explorer/',
