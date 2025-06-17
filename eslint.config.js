7a038cd888eee897cc629e81bc63f1a19d9d9689
Ethem-deli <del22021@byui.edu>
2025-06-17 15:30:23 +0300

    esling implemented and prettier added optionally

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 // eslint.config.js
 import js from '@eslint/js';
 import globals from 'globals';
-import reactPlugin from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
@@ -9,29 +8,20 @@ export default defineConfig([
         ignores: ['node_modules/**'],
     },
     {
-        files: ['src/js/**/*.js', 'src/js/**/*.jsx'],
-        plugins: {
-            react: reactPlugin,
-        },
+        files: ['src/js/**/*.js'],
         languageOptions: {
-            globals: { ...globals.browser },
             ecmaVersion: 2021,
             sourceType: 'module',
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true
-                }
-            }
-        },
-        settings: {
-            react: {
-                version: 'detect'
-            }
+            globals: {
+                ...globals.browser,
+            },
         },
         rules: {
             'no-unused-vars': 'warn',
-            semi: ['error', 'always'],
-            // add other rules here
-        }
-    }
+            'no-console': 'off',
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'indent': ['error', 2],
+        },
+    },
 ]);
